d4f680c547779126f60a9831049b6f8aa92b1b6b
Corentin-Vidick <corentin.vidick@gmail.com>
2023-02-03 11:50:31 +0000

    Create first skeleton template for bookings

--- bookings/models.py
+++ bookings/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# class Sessions(models.Model):
-#     title = models.CharField(max_length=20, unique=True)
-#     type = models.
+
+class SessionsIndividual(models.Model):
+    # might need DateField for date
+    days = models.CharField(max_length=20, null=False, blank=False)
+    # or DateTimeField to group date and time
+    times = models.CharField(max_length=20, null=False, blank=False)
+    confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        # to be modified to return proper string with info necessary
+        return self.days
--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import SessionsIndividual
+
 
 # Create your views here.
 def get_bookings_sessions(request):
-    return render(request, "bookings/bookings_list.html")
+    sessions = SessionsIndividual.objects.all()
+    context = {
+        'sessions': sessions
+    }
+
+    return render(request, "bookings/bookings_list.html", context)
