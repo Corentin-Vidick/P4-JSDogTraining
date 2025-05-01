410a6b07502422fa07b9762921e056611d767d03
Corentin-Vidick <111424947+Corentin-Vidick@users.noreply.github.com>
2025-05-01 21:01:32 +0000

    Debug True

--- jstraining/settings.py
+++ jstraining/settings.py
@@ -97,17 +97,17 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Use for testing
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Use for preview
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
- }
+# DATABASES = {
+#      'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+#  }
 
 
 # Password validation
