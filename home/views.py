f6d96b2e5bfd3844387970d12e1e5116badc7b76
Corentin-Vidick <111424947+Corentin-Vidick@users.noreply.github.com>
2025-05-03 21:32:49 +0000

    Short dated stock diplay on home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.views import generic, View
+import datetime
+
+from stock.models import Product, PackedStock, BulkStock
 
 
 def home(request):
     """
-    Contact form success confirmation
+    Displays short-dated stock for easy visualisation
     """
-    return render(request, "home/home.html")
+    print("Displaying short dated stock")
+    packed_stock = PackedStock.objects.all().order_by('expiry_date')[:3]
+    if packed_stock.exists():
+        print(packed_stock)
+    bulk_stock = BulkStock.objects.all().order_by('expiry_date')[:3]
+    if bulk_stock.exists():
+        print(bulk_stock)
+    #         for stock in bulk_stock:
+    #             if stock.quantity > 0:    
+    #                 first_bulk_stock = stock
+    #                 print(first_bulk_stock)
+    #                 break
+    return render(request, "home/home.html", {
+        'first_packed_stock': packed_stock,
+        'first_bulk_stock': bulk_stock,})
